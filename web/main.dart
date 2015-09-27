fccf4a2a28c700db022268245a27efa2cc2e4863
klikini <klikini@robiotic.net>
2015-09-26 23:01:52 -0500

    better init state

--- web/main.dart
+++ web/main.dart
@@ -12,6 +12,13 @@ part "package:TicTacToe3/ui.dart";
 Random rand = new Random();
 
 void main() {
+  window.console.warn(
+      "The world is not perfect. "
+      "Expect 404s of script files: "
+      "main.dart.js is missing when running Dart code, "
+      "and main.dart is missing when deployed."
+  );
+
   // Initialize cells
   for (int i = 0; i <= 15; i++) {
     new Cell(i);
