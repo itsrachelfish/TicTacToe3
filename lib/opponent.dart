c3175d6e1957eca9a15b5dec809569d14acecd35
klikini <klikini@robiotic.net>
2015-10-17 11:11:43 -0500

    hard mode (kind of)

--- lib/opponent.dart
+++ lib/opponent.dart
@@ -25,14 +25,77 @@ class Opponent {
 	}
 
 	int nextMove_easy() {
+		// List of empty indices
 		List<int> emptyCells = Grid.emptyCellsList;
+		// Random empty cell
 		int index = rand.nextInt(emptyCells.length);
+		// Index of the chosen cell
 		return emptyCells[index];
 	}
 
 	int nextMove_hard() {
-		// TODO: algorithm to find the best move
-		return nextMove_easy();
+		// Where to place the move
+		int index;
+		// List of empty indices
+		List<int> emptyCells = Grid.emptyCellsList;
+
+		// Check a list of cells for any blanks, and will return the index of a blank
+		// iff there is ONE blank (no more, no less)
+		int checkForSpot(List<int> indices) {
+			// Count blanks
+			int blanks = indices.where((int index) => Grid.cells[index].state == Player.NULL).toList().length;
+			if (blanks == 1) {
+				// One blank
+				return indices.where((int index) => Grid.cells[index].state == Player.NULL).toList().first;
+			} else {
+				// More or less than one blank
+				return -1;
+			}
+		}
+
+		// Check a set of lists of cells for blanks
+		Map<int, int> checkMultilevelSpot(Map<int, List<int>> indices) {
+			// Return value
+			Map<int, int> result = {};
+			// Go through each list of cells
+			indices.forEach((int id, List<int> cellIndices) {
+				// Store the list id and the blank spot value in the return value
+				result.addAll(({
+					id: checkForSpot(cellIndices)
+				}));
+			});
+			// Return the return value
+			return result;
+		}
+
+		// Go through the results of the checks and look for possible moves
+		int getMove(Map<int, int> data) {
+			for (int id in data.keys) {
+				int value = data[id];
+				if (value != -1) {
+					return value;
+				}
+			}
+			return -1;
+		}
+
+		Map<int, int> rows = checkMultilevelSpot(Grid.rows);
+		Map<int, int> cols = checkMultilevelSpot(Grid.cols);
+		Map<int, int> diags = checkMultilevelSpot(Grid.diags);
+
+		int rowMove = getMove(rows);
+		int colMove = getMove(cols);
+		int diagMove = getMove(diags);
+
+		if (rowMove != -1) {
+			return rowMove;
+		} else if (colMove != -1) {
+			return colMove;
+		} else if (diagMove != -1) {
+			return diagMove;
+		} else {
+			return nextMove_easy();
+		}
 	}
 
 	bool get isNull => (_player == Player.NULL || _difficulty == Difficulty.NULL);
